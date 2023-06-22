fe3024dc1c1a32b78cce4a21deee1acee8a7b142
Walteroriga44 <walteroriga44@gmail.com>
2023-06-22 05:46:50 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test that the function positive or negative gives the correct output
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
