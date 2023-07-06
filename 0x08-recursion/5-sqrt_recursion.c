071c5a13ddfcab022afc9a0fddadb077005e2beb
Walteroriga44 <walteroriga44@gmail.com>
2023-07-06 16:01:39 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,63 @@
 #include "main.h"
 #include <stdio.h>
-int _sqrt_recursion(int n, int i)
+/**
+ * calculate_sqrt - Recursive helper function to calculate the square root
+ * @n: The number for which the square root is to be calculated
+ * @start: The starting point for the search range
+ * @end: The ending point for the search range
+ *
+ * Return: The square root of n if it is a perfect square, -1 otherwise
+ */
+int calculate_sqrt(int n, int start, int end);
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: target number
+ * _sqrt_recursion - Calculates the square root of a number
+ * @n: The number for which the square root is to be calculated
  *
- * Return: n, -1
+ * Return: The square root of n if it is a perfect square, -1 otherwise
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+
+	return (calculate_sqrt(n, 1, n));
 }
 
 /**
- * _sqrt - calculates natural square root.
- * @n: number to calculate square root
- * i: iterate number
+ * calculate_sqrt - Recursive helper function to calculate the square root
+ * @n: The number for which the square root is to be calculated
+ * @start: The starting point for the search range
+ * @end: The ending point for the search range
  *
- * Return: natural square root
+ * Return: The square root of n if it is a perfect square, -1 otherwise
  */
-int _sqrt(int n, int i)
+int calculate_sqrt(int n, int start, int end)
 {
-	int sqrt = i * i;
-
-	if (sqrt > n)
+	if (start > end)
+	{
 		return (-1);
+	}
 
-	if (sqrt == n)
-		return (i);
+	int mid = (start + end) / 2;
 
-
-	return (_sqrt(n, i + 1));
+	if (mid == n / mid && n % mid == 0)
+	{
+		return (mid);
+	}
+	else if (mid > n / mid)
+	{
+		return (calculate_sqrt(n, start, mid - 1));
+	}
+	else
+	{
+		return (calculate_sqrt(n, mid + 1, end));
+	}
 }
