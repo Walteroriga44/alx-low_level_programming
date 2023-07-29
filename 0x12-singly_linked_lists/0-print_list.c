a69594e16c3ddebb193631dad46bac9ffcf49f88
Walteroriga44 <walteroriga44@gmail.com>
2023-07-29 13:13:21 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "list.h"
+#include "lists.h"
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: ptr to the ls
@@ -27,7 +27,7 @@ size_t print_list(const list_t *h)
 
 		if (h != NULL)
 		{
-			printf(", ")
+			printf(", ");
 		}
 	}
 	printf("]\n");
