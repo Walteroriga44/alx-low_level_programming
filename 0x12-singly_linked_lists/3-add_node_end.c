902378a7fc7f87d20bf8dba0328234d93ca68676
Walteroriga44 <walteroriga44@gmail.com>
2023-07-30 17:11:18 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,6 +12,10 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *current = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
 
 	if (str == NULL)
 		return (NULL);
