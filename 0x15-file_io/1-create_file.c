f40bf7c6573cc82be3c55c4481660baf0525eec8
Walteroriga44 <walteroriga44@gmail.com>
2023-09-04 09:09:35 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * create_file - create a file and write text content to it.
+ *
+ * @filename: The name of the file to create.
+ * @text_content: The NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+	ssize_t bytes_written;
+	int len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (file_descriptor == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(file_descriptor, text_content, len);
+
+		if (bytes_written == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+
+	close(file_descriptor);
+	return (1);
+}
